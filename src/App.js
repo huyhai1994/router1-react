af1c621e9f25be64dece1b451ff31d44df9e72e6
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-23 05:41:24 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link, Route, Routes} from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import React from "react";
+import Contract from "./components/Contract";
 
 function App() {
     return (<div>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contract/>}/>
+        </Routes>
     </div>);
 }
 
